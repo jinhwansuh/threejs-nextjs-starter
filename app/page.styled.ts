207e492453bd5d5daa52166e744b52d4c815bfc9
jinhwansuh <nick02190219@gmail.com>
2024-01-28 01:27:16 +0900

    feat: global css, viewer

---
+++ app/page.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  width: 100%;
+  height: 100%;
+`;
+
+export const CanvasWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: none;
+`;
